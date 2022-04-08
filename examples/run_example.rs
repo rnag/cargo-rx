7d9acc6e55a75f61b5ab847af6315529f10855fc
Ritvik Nag <rnag@collegeboard.org>
2022-04-08 13:57:24 -0400

    update example to pass in more options

--- examples/run_example.rs
+++ examples/run_example.rs
@@ -14,7 +14,19 @@ fn main() {
         args: Vec::from(args.map(String::from)),
         input_args: false,
         cargo: cargo_options::CommonOptions {
+            // enable verbose output
             verbose: 1,
+            // uncomment to build in `--release` mode
+            // release: true,
+            jobs: Some(2),
+            profile: Some("release".to_owned()),
+            features: vec!["feature-1".to_owned()],
+            no_default_features: true,
+            ignore_rust_version: true,
+            color: Some("always".to_owned()),
+            locked: true,
+            offline: true,
+            config: vec!["my_cfg='test'".to_owned()],
             ..Default::default()
         },
         ..Default::default()
